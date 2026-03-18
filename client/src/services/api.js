d1b1f573127a433c0c25e24d0695ec04f31aae16
Chetan Kumar <118592926+Chetan231@users.noreply.github.com>
2026-03-19 02:00:10 +0530

    Update api.js

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -3,7 +3,7 @@
  */
 import axios from 'axios';
 
-const API_BASE = `${import.meta.env.VITE_API_URL}/api`;
+const API_BASE ="http://localhost:5000/api";
 
 const api = axios.create({
   baseURL: API_BASE,
